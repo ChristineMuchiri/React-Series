74a49045596fb1e166e23f597bb7f84a16eac9c6
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-15 16:03:32 +0300

    child components

--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -1,10 +1,22 @@
 import { UserProfile } from "./components/UserProfile";
 
 export default function App() {
+    const callMe = () => { 
+        console.log("Hello");
+    };
     return (
         <div>
             <h1>Root Component</h1>
-            <UserProfile Age={20} isLoggedIn={true} />
+            <UserProfile
+                username='bob'
+                isLoggedIn={true}
+                favouriteFoods={[
+                    {
+                        name: "sushi"
+                    }
+                ]}
+                callMe={callMe}
+            />
         </div>
     );
 
--- react-tutorial/src/components/UserProfile.jsx
+++ react-tutorial/src/components/UserProfile.jsx
@@ -1,13 +1,22 @@
+import PropTypes from 'prop-types';
 import { UserFavouriteFood } from "./UserFavouriteFood";
 import { UserUsername } from "./UserUsername";
 
 export function UserProfile(props) {
+    console.log(props);
+    props.callMe();
+
     return (
         <div id='user-profile'>
-            <UserUsername username="bob" trimmedUsername='ken'/>
+            <UserUsername username={props.username} />
             <b>Age:</b>
             <span>{props.Age }</span>
             <UserFavouriteFood />
         </div>
     )
+}
+UserProfile.propTypes = {
+    username: PropTypes.string.isRequired,
+    Age: PropTypes.number.isRequired,
+    callMe: PropTypes.func.isRequired,
 }
